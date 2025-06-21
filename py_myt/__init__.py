cc31ab3ba7f40ed95237a7a061865f595048cdb0
Coo <kuqitt1@163.com>
2025-06-21 13:44:50 +0800

    Initial commit: MYT SDK with complete API client support

---
+++ py_myt/__init__.py
@@ -0,0 +1,24 @@
+"""MYT SDK - 魔云腾SDK通用包"""
+
+__version__ = "1.0.0"
+__author__ = "MYT Team"
+__email__ = "support@moyunteng.com"
+
+from .sdk_manager import MYTSDKManager
+from .api_client import MYTAPIClient, create_client
+from .exceptions import (
+    MYTSDKError,
+    MYTSDKDownloadError,
+    MYTSDKProcessError,
+    MYTSDKFileError
+)
+
+__all__ = [
+    'MYTSDKManager',
+    'MYTAPIClient',
+    'create_client',
+    'MYTSDKError',
+    'MYTSDKDownloadError', 
+    'MYTSDKProcessError',
+    'MYTSDKFileError'
+]
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="myt-sdk",
+    version="1.0.0",
+    author="MYT Team",
+    author_email="support@moyunteng.com",
+    description="MYT SDK - 魔云腾SDK通用包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moyunteng/myt-sdk",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=21.0",
+            "flake8>=3.8",
+            "mypy>=0.800",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "myt-sdk=py_myt.cli:main",
+        ],
+    },
+)
